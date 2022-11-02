b5b4877032264b641ffbec157c519fed94f9bc10
U480401 <adrissi@bptech.ma>
2022-11-02 14:28:12 +0100

    ====> update base route

--- src/main/java/com/referentiel/zawya/controller/ZawyaController.java
+++ src/main/java/com/referentiel/zawya/controller/ZawyaController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/zawyas")
+@RequestMapping("/referentiel/zawyas")
 @CrossOrigin(value = "*")
 public class ZawyaController {
 
